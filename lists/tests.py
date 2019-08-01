7685ab4b88a90fb589fbea196bce56c6904f23e3
glowingEdge <glowingEdge.com>
2019-08-01 21:04:06 +0900

    add FT for the additional table row input

--- lists/tests.py
+++ lists/tests.py
@@ -15,13 +15,11 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_templates(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+    def test_home_page_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
