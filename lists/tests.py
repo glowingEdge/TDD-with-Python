61ade3a2e77652d7396d1490b3388661e40a2004
glowingEdge <glowingEdge.com>
2019-08-01 21:40:25 +0900

    model for list items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
+from lists.models import Item
 
 # for initial test django TestCase
 #
@@ -23,3 +19,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
         self.assertIn('신규 작업 아이템', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
+
