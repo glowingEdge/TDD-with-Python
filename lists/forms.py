4fe4902a19190432a9c3067b0e0a3a6a52955e73
glowingEdge <ddukfate213@gmail.com>
2020-03-02 15:45:05 +0900

    use form's own save method

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,9 @@ from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
+
 class ItemForm(forms.models.ModelForm):
+
     class Meta:
         model = Item
         fields = ('text',)
@@ -17,3 +19,7 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
